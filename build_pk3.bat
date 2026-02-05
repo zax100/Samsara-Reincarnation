60c87c75163890507693740382252ec6010da2f3
cubebert-github <cubebertoffical@gmail.com>
2026-02-05 06:43:14 -0800

    Added back Doom 64 CE support (still WIP)
    -Doom 64 CE should be playable, but the Acid Demon and the Stalker need to be ported over, along with any bugs that need to be fixed

--- build_pk3.bat
+++ build_pk3.bat
@@ -1,7 +1,7 @@
 @echo off
 
 REM file name of the built PK3, feel free to edit - Stolen from COG lol
-set pk3name="SamsaraReincarnation_v2.1-Dev-Build"
+set pk3name="SamsaraReincarnation_v2.2.1-Dev-Build"
 
 
 
@@ -10,4 +10,4 @@ REM !!! DO NOT EDIT ANYTHING BEYOND THIS POINT !!!!
 REM !!! DO NOT EDIT ANYTHING BEYOND THIS POINT !!!!
 
 if exist %pk3name%.pk3 del %pk3name%.pk3
-7za.exe a -r -x!.git\ -x!.github\ -x!.vscode\ -x!*.gitattributes -x!*.gitignore -x!src\ -x!psd\ -x!resources\ -x!filter\doom.ce.doom64\ -xr!*.bat -xr!README.md -xr!*.pk3 -xr!*.dbs -xr!*.exe -xr!*.dll -xr!*.bak -xr!*.backup* -xr!*.workspace -xr!*.psd -xr!*.blend -xr!*.lnk -xr!*.wl6 -xr!*.autosave* -xr!*.ps1* -ssw -tzip -mx7 .\%pk3name%.pk3 "*"
+7za.exe a -r -x!.git\ -x!.github\ -x!.vscode\ -x!*.gitattributes -x!*.gitignore -x!src\ -x!psd\ -x!resources\ -xr!*.bat -xr!README.md -xr!*.pk3 -xr!*.dbs -xr!*.exe -xr!*.dll -xr!*.bak -xr!*.backup* -xr!*.workspace -xr!*.psd -xr!*.blend -xr!*.lnk -xr!*.wl6 -xr!*.autosave* -xr!*.ps1* -ssw -tzip -mx7 .\%pk3name%.pk3 "*"
